4b10cc0f7f0d45fe4427f9520e287cbf19fac05d
Juhwan Park <juhwanpark421@gmail.com>
2018-09-11 18:30:37 +0900

    color Theme change

--- src/config/colorTheme.js
+++ src/config/colorTheme.js
@@ -1,33 +1,35 @@
 // Imports
 import { COLOR } from 'react-native-material-ui';
 
+const mainColor = 'blueGrey'
+
 //--------- NATIVE-BASE THEME
 // header
-export const toolbarBtnColor = COLOR.blueGrey900
-export const toolbarDefaultBg = COLOR.blueGrey900
+export const toolbarBtnColor = COLOR[mainColor + '900']
+export const toolbarDefaultBg = COLOR[mainColor + '900']
 
 // color
-export const brandPrimary = COLOR.blueGrey500
-export const brandDark = COLOR.blueGrey900
-export const brandLight = COLOR.blueGrey200
+export const brandPrimary = COLOR[mainColor + '500']
+export const brandDark = COLOR[mainColor + '900']
+export const brandLight = COLOR[mainColor + '200']
 
 // container
-export const containerBgColor = COLOR.blueGrey100
+export const containerBgColor = COLOR[mainColor + '100']
 
 // card
-export const cardDefaultBg = COLOR.blueGrey800
-export const cardBorderColor = COLOR.blueGrey700
+export const cardDefaultBg = COLOR[mainColor + '800']
+export const cardBorderColor = COLOR[mainColor + '700']
 
 // text
-export const textColor =  COLOR.blueGrey50
+export const textColor =  COLOR[mainColor + '50']
 
 
 //--------- APP THEME
 // header
-export const headerBg = COLOR.blueGrey900
-export const headerTitleColor = COLOR.blueGrey50
-export const headerIconsColor = COLOR.blueGrey50
-export const headerTintColor = COLOR.blueGrey50 //arrow
+export const headerBg = COLOR[mainColor + '900']
+export const headerTitleColor = COLOR[mainColor + '50']
+export const headerIconsColor = COLOR[mainColor + '50']
+export const headerTintColor = COLOR[mainColor + '50'] //arrow
 
 // Card
 export const cardBackgroundColor = containerBgColor // match with
